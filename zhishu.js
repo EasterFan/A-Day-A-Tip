69cf5c996c435f08e413452c24bd0d0a1ed420ab
EasterFan <fan.easter@gmail.com>
2017-08-07 15:14:03 +0800

    100以内所有质数

---
+++ zhishu.js
@@ -0,0 +1,28 @@
+/**
+ * 求100以内所有质数
+ * Created by easter on 17-7-24.
+ */
+
+'use strict'
+
+function zhishu()
+{
+   let arr = [];
+   for (let i = 3;i<100;i++)
+   {
+       for (let j =2; j<i;j++)
+       {
+           if(i % j == 0)
+               break;
+           if(i - 1 == j)
+               arr.push(i);
+
+       }
+   }
+
+   return arr;
+}
+
+let arr = zhishu();
+let result = "100以内的质数有：" + arr;
+console.log(result);
